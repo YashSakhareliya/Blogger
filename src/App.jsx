716443cb1677eae2b761936c4dd7c29ec2fe3c79
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-02-16 20:59:06 +0530

    outlet added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react"
 import {login, logout} from "./store/authSlice"
 import authService from "./appwrite/auth"
 import {Header, Footer} from "./components"
+import { Outlet } from "react-router"
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -26,6 +27,9 @@ function App() {
     <div className="min-h-screen w-full flex flex-wrap content-between bg-gray-400">
       <div className="w-full block">
         <Header />
+        <main>
+          <Outlet />
+        </main>
         <Footer />
       </div>
     </div>
