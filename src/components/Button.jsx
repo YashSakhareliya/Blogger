4a28196b214b283ebe41c316e780f5475317876d
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-02-20 17:29:20 +0530

    Button components DOne

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
-function Button() {
+function Button({
+    children,
+    type="button",
+    bgColor="bg-blue-500",
+    textColor="text-white",
+    className = "",
+    ...props
+}) {
   return (
     <button 
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    ></button>
+    className={`px-4 py-2 duration-200 rounded-lg ${bgColor} ${textColor} ${className}`}
+    type={type}
+    {...props}
+    >
+    {children}
+    </button>
   )
 }
 
