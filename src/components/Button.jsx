2863aa0726e2dc7f2f65b7584204f9aca4c95047
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-02-20 17:18:14 +0530

    Button updated

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Button() {
+  return (
+    <button 
+    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    ></button>
+  )
+}
+
+export default Button
