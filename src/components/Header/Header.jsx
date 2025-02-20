9dfd2ea63c0c83f20c733264aec40260e955e394
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-02-20 17:16:29 +0530

    Header Done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -36,9 +36,35 @@ function Header() {
     },
   ]
   return (
-    <div>
-      Header
-    </div>
+    <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link>
+            <Logo width='70px' />
+            </Link>
+          </div>
+
+          <ul className='flex ml-auto'>
+            {navItems.map((item)=>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                  className='inline-block px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                  onClick={() => navigate(item.slug)}
+                  >{item.name}</button>
+                </li>
+              ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+      </Container>
+    </header>
   )
 }
 
