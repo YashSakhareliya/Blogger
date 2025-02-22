36baa99e6b6a5a644a2992403341ecf5246d4ad6
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-02-22 14:51:35 +0530

    post form taking value from form done

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, {useCallback} from 'react'
+import { useForm } from 'react-hook-form'
+import { Button, Input, RTE, Select } from "../index";
+import appwriteService from "../../appwrite/config";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
-export default function PostForm() {
+export default function PostForm({post}) {
+    const { register, handleSubmit, setValue, watch, control, getValues } = useForm({
+        defaultValues: {
+            title: post?.title || "",
+            slug: post?.$id || "",
+            content: post?.content || "",
+            status: post?.status || "active",
+        },
+    });
+
+    const navigate = useNavigate();
+    const userData = useSelector((state) => state.auth.userData);
+    
   return (
     <div>
       
