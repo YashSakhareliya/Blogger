053a2133d0e11cf04d1528c94632df669c97453c
Yash sakhareliya <sakhareliyayash@gmail.com>
2025-02-25 08:16:34 +0530

    bug fix

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import logo from '../assets/60736.png'
 
 function Logo({width = '80px'}) {
   return (
     <div className='flex gap-4 items-center content-center'>
-      <img src="../src/public/60736.png" alt="logo image" width="40px"/>
+      <img src={logo} alt="logo image" width="40px"/>
       <span className='font-medium text-2xl'>Blogger</span>
     </div>
   )
